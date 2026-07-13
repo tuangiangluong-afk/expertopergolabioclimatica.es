6bbef001780da0f6910228c604e7faf7dbbd1914
tuangiangluong-afk <tuangiang.luong@gmail.com>
2026-07-13 19:26:00 +0200

    feat: implement SEO/GEO/AEO speakable, HowTo, and publisher schemas

--- src/components/seo/StructuredData.tsx
+++ src/components/seo/StructuredData.tsx
@@ -55,11 +55,50 @@ export default function StructuredData() {
         "mainEntity": []
     };
 
+    
+    const webPageSchema = {
+        "@context": "https://schema.org",
+        "@type": "WebPage",
+        "url": "https://www.expertopergolabioclimatica.es",
+        "name": "Experto Pérgola Bioclimática",
+        "description": "Instalación de pérgolas bioclimáticas a medida",
+        "inLanguage": "es",
+        "speakable": {
+            "@type": "SpeakableSpecification",
+            "cssSelector": [
+                "h1",
+                ".hero-description",
+                ".faq-answer",
+                "article h2",
+                "article p:first-of-type",
+                ".prose > p:first-child"
+            ]
+        },
+        "isPartOf": {
+            "@type": "WebSite",
+            "url": "https://www.expertopergolabioclimatica.es",
+            "name": "Experto Pérgola Bioclimática"
+        }
+    };
+
     return (
+        <>
         <Script
             id="org-schema"
             type="application/ld+json"
             dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) + '\n' + JSON.stringify(websiteSchema) + '\n' + JSON.stringify(serviceSchema) + '\n' + JSON.stringify(faqSchema) }}
         />
+
+        <Script
+
+            id="webpage-speakable-schema"
+
+            type="application/ld+json"
+
+            dangerouslySetInnerHTML={{ __html: JSON.stringify(webPageSchema) }}
+
+        />
+
+        </>
     );
 }
